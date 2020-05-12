4fdf5344efaf3f1244a140bedbdedffe97d73a37
lucialee <booungy@gmail.com>
2020-05-13 00:10:07 +0900

    백준 : 레벨1 - 입출력과 사칙연산. 3번 고양이

--- baek-jun/level1/level1.js
+++ baek-jun/level1/level1.js
@@ -1,11 +1,72 @@
 function num1() {
+    /**
+     * Hello World
+     문제
+     Hello World!를 출력하시오.
+
+     입력
+     없음
+
+     출력
+     Hello World!를 출력하시오.
+     * */
     console.log("Hello World!")
 }
 function num2() {
+    /**
+     * We love kriii
+     문제
+     ACM-ICPC 인터넷 예선, Regional, 그리고 World Finals까지 이미 2회씩 진출해버린 kriii는 미련을 버리지 못하고 왠지 모르게 올 해에도 파주 World Finals 준비 캠프에 참여했다.
+
+     대회를 뜰 줄 모르는 지박령 kriii를 위해서 격려의 문구를 출력해주자.
+
+     입력
+     없음
+
+     출력
+     두 줄에 걸쳐 "강한친구 대한육군"을 한 줄에 한 번씩 출력한다.
+     * */
     console.log("강한친구 대한육군")
     console.log("강한친구 대한육군")
 }
+function num3() {
+    /**
+     * 고양이
+     문제
+     아래 예제와 같이 고양이를 출력하시오.
+
+     입력
+     없음.
+
+     출력
+     \    /\
+     )  ( ')
+     (  /  )
+     \(__)|
+     * */
+    console.log("\\    /\\")
+    console.log(" )  ( ')")
+    console.log("(  /  )")
+    console.log(" \\(__)|")
+//또는. 띄어쓰기 조심
+    console.log(`\\    /\\
+ )  ( ')
+(  /  )
+ \\(__)|`);
+}
+
 function num5() {
+    /**
+     * A+B
+     문제
+     두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+     입력
+     첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+     출력
+     첫째 줄에 A+B를 출력한다.
+     * */
     const fs = require('fs');
     const input = fs.readFileSync('/dev/stdin').toString().split(' ');
     const A = parseInt(input[0]);
@@ -179,4 +240,4 @@ function num11() {
 }
 
 
-// console.log(num11(472, 385))
+console.log(num3())
