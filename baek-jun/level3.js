b98f2205de33b2e69391d9749333f4ca976ee9d1
lucialee <booungy@gmail.com>
2020-05-14 17:13:31 +0900

    백준 : 레벨3 - for문. 3번 합

--- baek-jun/level3.js
+++ baek-jun/level3.js
@@ -86,20 +86,32 @@ function Num2() {
 
 function Num3() {
     /**
-     * A×B
+     * 합 (8393 번)
+
+     문제
+     n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+
+     입력
+     첫째 줄에 n (1 ≤ n ≤ 10,000)이 주어진다.
+
+     출력
+     1부터 n까지 합을 출력한다.
 
      예제
-     input
-     return
+     input 3
+     return 6
      * */
 
     const fs = require('fs');
-    const input = fs.readFileSync('/dev/stdin').toString().split(' ');
-    const A = parseInt(input[0]);
-    const B = parseInt(input[1]);
+    const input = Number(fs.readFileSync('/dev/stdin'));
 
-    console.log(A * B);
+    let result = 0;
+    for (let i = 1; i <= input; i++) {
+        result = result + i
+    }
+    console.log(result);
 }
+// Num3(3);
 
 function Num4() {
     /**
