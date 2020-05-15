14cbd183bb0acd58775f62766fda0cece71d40e8
lucialee <booungy@gmail.com>
2020-05-15 17:48:52 +0900

    백준 : 레벨3 - for문. 7번 A+B -7

--- baek-jun/level3.js
+++ baek-jun/level3.js
@@ -239,21 +239,43 @@ function Num6() {
 
 function Num7() {
     /**
-     * A×B
+     * A+B - 7 (11021번)
+
+     문제
+     두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+     입력
+     첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+
+     각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+     출력
+     각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 테스트 케이스 번호는 1부터 시작한다.
 
      예제
      input
+     5
+     1 1
+     2 3
+     3 4
+     9 8
+     5 2
      return
+     Case #1: 2
+     Case #2: 5
+     Case #3: 7
+     Case #4: 17
+     Case #5: 7
      * */
 
-    const fs = require('fs');
-    const input = fs.readFileSync('/dev/stdin').toString().split(' ');
-    const A = parseInt(input[0]);
-    const B = parseInt(input[1]);
+    const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
 
-    console.log(A * B);
+    for (let i = 1; i <= Number(input[0]); i ++){
+        const values = input[i].split(' ');
+        console.log(`Case #${i}: ${Number(values[0]) +Number(values[1])}`)
+    }
 }
-
+Num7([ "5",    "1 1",    "2 3",    "3 4",    "9 8",    "5 2"])
 function Num8() {
     /**
      * A×B
