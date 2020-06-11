df3736396e205956b2c0386fe9b4cc8f315d53df
lucialee <booungy@gmail.com>
2020-06-12 08:44:06 +0900

    백준 : 레벨6 - 1차원 배열. 최댓값 (2562번)

--- baek-jun/level6_array.js
+++ baek-jun/level6_array.js
@@ -39,4 +39,53 @@ function Que1 (input) {
     console.log(`${min} ${max}`)
 }
 
-Que1(['5', '20 10 35 30 7'])
+function Que2 (input) {
+    /**
+     * 	최댓값 (2562번)
+
+     문제
+     9개의 서로 다른 자연수가 주어질 때, 이들 중 최댓값을 찾고 그 최댓값이 몇 번째 수인지를 구하는 프로그램을 작성하시오.
+
+     예를 들어, 서로 다른 9개의 자연수
+     3, 29, 38, 12, 57, 74, 40, 85, 61
+     이 주어지면, 이들 중 최댓값은 85이고, 이 값은 8번째 수이다.
+
+     입력
+     첫 째 줄부터 아홉 번째 줄까지 한 줄에 하나의 자연수가 주어진다. 주어지는 자연수는 100 보다 작다.
+
+     출력
+     첫째 줄에 최댓값을 출력하고, 둘째 줄에 최댓값이 몇 번째 수인지를 출력한다.
+
+     예제
+     input
+     3
+     29
+     38
+     12
+     57
+     74
+     40
+     85
+     61
+     return
+     85
+     8
+     * */
+
+        // const input = require('fs').readFileSync('/dev/stdin').toString().split("\n");
+    let max = parseInt(input)
+    let idx = 1
+    input.map((value, i) => {
+        value = parseInt(value)
+        if (max < value) {
+            max = value
+            idx = i + 1
+        }
+    })
+    console.log(max)
+    console.log(idx)
+}
+
+
+// Que1(['5', '20 10 35 30 7'])
+Que2([3, 29, 38, 12, 57, 74, 40, 85, 61])
