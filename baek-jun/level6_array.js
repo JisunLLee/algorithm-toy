e7d72fedace14db1238fd7fd08fa0d5292704361
lucialee <booungy@gmail.com>
2020-06-16 11:25:05 +0900

    백준 : 레벨6 - 1차원 배열. OX 퀴즈 (8958번).

--- baek-jun/level6_array.js
+++ baek-jun/level6_array.js
@@ -287,9 +287,60 @@ function Que5_3 () {
     console.log(sum / score.length)
 }
 
+function Que6 (input) {
+    /**
+     * 	OX 퀴즈 (8958번)
+
+     문제
+     "OOXXOXXOOO"와 같은 OX퀴즈의 결과가 있다. O는 문제를 맞은 것이고, X는 문제를 틀린 것이다.
+     문제를 맞은 경우 그 문제의 점수는 그 문제까지 연속된 O의 개수가 된다. 예를 들어, 10번 문제의 점수는 3이 된다.
+
+     "OOXXOXXOOO"의 점수는 1+2+0+0+1+0+0+1+2+3 = 10점이다.
+
+     OX퀴즈의 결과가 주어졌을 때, 점수를 구하는 프로그램을 작성하시오.
+
+     입력
+     첫째 줄에 테스트 케이스의 개수가 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있고, 길이가 0보다 크고 80보다 작은 문자열이 주어진다.
+     문자열은 O와 X만으로 이루어져 있다.
+
+     출력
+     각 테스트 케이스마다 점수를 출력한다.
+
+     예제
+     input
+     5
+     OOXXOXXOOO
+     OOXXOOXXOO
+     OXOXOXOXOXOXOX
+     OOOOOOOOOO
+     OOOOXOOOOXOOOOX
+
+     output
+     10
+     9
+     7
+     55
+     30
+
+     */
+    // const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+    for (let i = 1; i <= Number(input[0]); i++){
+        let straight = 0;
+        let score = 0;
+        input[i].split('').map(value => {
+            if (value === "O") straight++
+            else straight = 0
+            score += straight
+        })
+        console.log(score)
+    }
+
+}
+
 
 // Que1(['5', '20 10 35 30 7'])
 // Que2([3, 29, 38, 12, 57, 74, 40, 85, 61])
 // Que3(['150','266','427'])
 // Que4(['39', '40', '41', '42', '43', '44', '82', '83', '84', '85'])
-Que5(['3','1 10 5'])
+// Que5(['3','1 10 5'])
+Que6(['5', 'OOXXOXXOOO', 'OOXXOOXXOO', 'OXOXOXOXOXOXOX', 'OOOOOOOOOO', 'OOOOXOOOOXOOOOX'])
