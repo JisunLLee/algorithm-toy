c853d3776e8f5768458f707d3dce43f57567ed8e
lucialee <booungy@gmail.com>
2020-06-16 09:44:47 +0900

    백준 : 레벨6 - 1차원 배열. 평균 (1546번). * 주의 : 만약 sort를 한다면, 1, 10, 5 를 해 볼 것.

--- baek-jun/level6_array.js
+++ baek-jun/level6_array.js
@@ -183,7 +183,113 @@ function Que4 (input) {
     //solution3
     console.log(remainders.sort().filter((value, index) => value !== remainders[index+1]).length)
 }
+
+function Que5 (input) {
+    /**
+     * 	평 균(1546번)
+
+     문제
+     세준이는 기말고사를 망쳤다. 세준이는 점수를 조작해서 집에 가져가기로 했다.
+     일단 세준이는 자기 점수 중에 최댓값을 골랐다. 이 값을 M이라고 한다. 그리고 나서 모든 점수를 점수/M*100으로 고쳤다.
+     예를 들어, 세준이의 최고점이 70이고, 수학점수가 50이었으면 수학점수는 50/70*100이 되어 71.43점이 된다.
+     세준이의 성적을 위의 방법대로 새로 계산했을 때, 새로운 평균을 구하는 프로그램을 작성하시오.
+
+     입력
+     첫째 줄에 시험 본 과목의 개수 N이 주어진다. 이 값은 1000보다 작거나 같다. 둘째 줄에 세준이의 현재 성적이 주어진다.
+     이 값은 100보다 작거나 같은 음이 아닌 정수이고, 적어도 하나의 값은 0보다 크다.
+
+     출력
+     첫째 줄에 새로운 평균을 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-2 이하이면 정답이다.
+
+     예제
+     input
+     3
+     40 80 60
+
+     output
+     75.0
+
+     주의:
+     input
+     2
+     1 5 10
+     return
+     53.33
+     */
+
+    /**
+     * 메모리 : 7100 KB
+     * 시간 : 108ms
+     * */
+
+    // const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+
+    const score = input[1].split(' ').map(v => +v)
+    const high = Math.max(...score)
+
+    let sum = 0;
+    score.map( (value, index) => {
+        const newScore = Number(value) / high * 100
+        sum += Number(newScore.toFixed(2))
+    })
+    console.log(sum / score.length)
+}
+function Que5_1 (input) {
+    /**
+     * 메모리 : 7116 KB
+     * 시간 : 108ms
+     * */
+    // const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+    const length = Number(input[0])
+    const score = input[1].split(' ').sort(function (a, b) {
+        return b - a
+    })
+    const high = Number(score[0])
+
+    let sum = 0;
+    score.map( (value, index) => {
+        const newScore = Number(value) / high * 100
+        sum += Number(newScore.toFixed(2))
+    })
+    console.log(sum / length)
+}
+function Que5_2 () {
+    /**
+     * 메모리 : 7108 KB
+     * 시간 : 104ms
+     * */
+    const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+    const length = +input.shift();
+    const score = input.shift().split(' ').map(v => +v)
+    const high = Math.max(...score)
+
+    let sum = 0;
+    score.map( (value, index) => {
+        const newScore = value / high * 100
+        sum += Number(newScore.toFixed(2))
+    })
+    console.log(sum / length)
+}
+function Que5_3 () {
+    /**
+     * 메모리 : 7116 KB
+     * 시간 : 108ms
+     * */
+    const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+    const score = input[1].split(' ').map(v => +v)
+    const high = Math.max(...score)
+
+    let sum = 0;
+    score.map( (value, index) => {
+        const newScore = value / high * 100
+        sum += Number(newScore.toFixed(2))
+    })
+    console.log(sum / score.length)
+}
+
+
 // Que1(['5', '20 10 35 30 7'])
 // Que2([3, 29, 38, 12, 57, 74, 40, 85, 61])
 // Que3(['150','266','427'])
-Que4(['39', '40', '41', '42', '43', '44', '82', '83', '84', '85'])
+// Que4(['39', '40', '41', '42', '43', '44', '82', '83', '84', '85'])
+Que5(['3','1 10 5'])
