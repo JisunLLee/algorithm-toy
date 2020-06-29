5f1716617bff2af624829743a58e51a2320cc61e
lucialee <booungy@gmail.com>
2020-06-30 01:04:39 +0900

    백준 : 레벨6 - 1차원 배열. 평균은 넘겠지 (8958번). 제일 재미있었당.

--- baek-jun/level6_array.js
+++ baek-jun/level6_array.js
@@ -337,10 +337,77 @@ function Que6 (input) {
 
 }
 
+function Que7 (input) {
+    /**
+     * 	평균은 넘겠지 (8958번)
+
+     문제
+     대학생 새내기들의 90%는 자신이 반에서 평균은 넘는다고 생각한다. 당신은 그들에게 슬픈 진실을 알려줘야 한다.
+
+     입력
+     첫째 줄에는 테스트 케이스의 개수 C가 주어진다.
+
+     둘째 줄부터 각 테스트 케이스마다 학생의 수 N(1 ≤ N ≤ 1000, N은 정수)이 첫 수로 주어지고, 이어서 N명의 점수가 주어진다.
+     점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+
+     출력
+     각 케이스마다 한 줄씩 평균을 넘는 학생들의 비율을 반올림하여 소수점 셋째 자리까지 출력한다.
+
+     예제
+     input
+     5
+     5 50 50 70 80 100
+     7 100 95 90 80 70 60 50
+     3 70 90 80
+     3 70 90 81
+     9 100 99 98 97 96 95 94 93 91
+
+     output
+     40.000%
+     57.143%
+     33.333%
+     66.667%
+     55.556%
+
+     */
+
+        //각 케이스마다 한 줄씩 평균을 넘는 학생들의 비율을 반올림하여 소수점 셋째 자리까지 출력한다.
+        // 1. 평균 구하기
+        // 2. 평균 넘는 학생 비율 구하기  = (100 * 평균 넘는 학생 수) / 총 학생 수;
+        // 3. 소수점 셋째 자리 까지 출력 (반올림)
+
+    // const input = require('fs').readFileSync('/dev/stdin').toString().trim().split("\n");
+    input.shift()
+
+    input.map(value => {
+        const list = value.split(' ').map(v => +v)
+        const N = list.shift()
+
+        let sum = 0;
+
+        list.forEach(v => sum += v);
+        const evr = sum/N
+
+        let count = 0
+        let data = 0
+        list.forEach(value => {
+            if (value > evr) {
+                count++;
+                data += value
+            }
+        })
+        const answer =  (100 * count)/N;
+
+        console.log( `${answer.toFixed(3)}%`)
+    })
+
+}
+
 
 // Que1(['5', '20 10 35 30 7'])
 // Que2([3, 29, 38, 12, 57, 74, 40, 85, 61])
 // Que3(['150','266','427'])
 // Que4(['39', '40', '41', '42', '43', '44', '82', '83', '84', '85'])
 // Que5(['3','1 10 5'])
-Que6(['5', 'OOXXOXXOOO', 'OOXXOOXXOO', 'OXOXOXOXOXOXOX', 'OOOOOOOOOO', 'OOOOXOOOOXOOOOX'])
+// Que6(['5', 'OOXXOXXOOO', 'OOXXOOXXOO', 'OXOXOXOXOXOXOX', 'OOOOOOOOOO', 'OOOOXOOOOXOOOOX'])
+// Que7(['5', '5 50 50 70 80 100', '7 100 95 90 80 70 60 50', '3 70 90 80', '3 70 90 81', '9 100 99 98 97 96 95 94 93 91'])
