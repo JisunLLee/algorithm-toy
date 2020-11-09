0a2fe5ad0b4f8d96a835c65958db4bb94e891620
lucialee <booungy@gmail.com>
2020-11-09 16:30:05 +0900

    백준 : 레벨8 - 문자열. 10번 그룹단어 체커 (1316번). 레벨8 완료

--- baek-jun/level8_string.js
+++ baek-jun/level8_string.js
@@ -500,6 +500,63 @@ function Que9(input) {
 
 }
 
+function Que10(input) {
+    /**
+     * 그룹 단어 체커 (1316번)
+     *
+     * 문제
+     그룹 단어란 단어에 존재하는 모든 문자에 대해서, 각 문자가 연속해서 나타나는 경우만을 말한다.
+     예를 들면, ccazzzzbb는 c, a, z, b가 모두 연속해서 나타나고, kin도 k, i, n이 연속해서 나타나기 때문에 그룹 단어이지만,
+     aabbbccb는 b가 떨어져서 나타나기 때문에 그룹 단어가 아니다.
+
+     단어 N개를 입력으로 받아 그룹 단어의 개수를 출력하는 프로그램을 작성하시오.
+
+     입력
+     첫째 줄에 단어의 개수 N이 들어온다. N은 100보다 작거나 같은 자연수이다. 둘째 줄부터 N개의 줄에 단어가 들어온다.
+     단어는 알파벳 소문자로만 되어있고 중복되지 않으며, 길이는 최대 100이다.
+
+     출력
+     첫째 줄에 그룹 단어의 개수를 출력한다.
+
+     예제 1
+     input
+     3
+     happy
+     new
+     year
+
+     output
+     3
+
+     예제 2
+     input
+     4
+     aba
+     abab
+     abcabc
+     a
+
+     output
+     1
+     **/
+    // const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+    // 아이디어. 앞에거랑 다르면 추가. 마지막 나온 목록 랭스 가지고 있기. 중복 지운 랭스랑 마지막 랭스랑 비교해서 숫자가 다르면 카운트 +0, 같으면 카운트 +1
+    let count = 0
+    input.shift();
+
+    input.map(value => {
+        let last_word = ''
+        const data = value.split('')
+        let result_data = []
+        data.map((v, i) =>{
+            if(v !== last_word) result_data.push(v)
+            last_word = v
+        })
+        if (result_data.length === Array.from(new Set(result_data)).length) count++
+    })
+    console.log(count)
+
+}
 // Que1("9")
 // Que2(['25', '10987654321'])
 // Que3('baekjoon')
@@ -518,4 +575,7 @@ function Que9(input) {
 // Que9(['c','='])
 // Que9(['l','j','e','s','=','n','j','a','k'])
 // Que9(['d','d','z','=','z','='])
-Que9('ddz=z=')
+// Que9('ddz=z=')
+// Que10(['3','happy','new','year'])
+// Que10(['4', 'aba', 'abab', 'abcabc', 'a'])
+// Que10 (['1','aaa'])
