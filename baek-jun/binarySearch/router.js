7a530f462df30322b5a92296a7f0bd96645480b5
lucialee <booungy@gmail.com>
2021-10-29 21:54:28 +0900

    백준. 2110번 공유기 설치 문제 풀이 완. 한번 더 풀어봤으면 좋겠다

--- baek-jun/binarySearch/router.js
+++ baek-jun/binarySearch/router.js
@@ -4,22 +4,41 @@
 //const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 function solution(input){
-    const N = Number(input[0].split(' ')[0]);
-    const C = Number(input[0].split(' ')[1]);
-    input.shift();
+    const [N, C]= input.shift().split(' ').map(Number);
 
-    input.sort((a, b) => a - b);
-    let start = 1;
-    let end = input[input.length-1];
+    const house = input.map(Number).sort((a, b) => a - b);
 
-    while(start <= end){
-        const mid = Math.floor((start + end) / 2);
+    let min = 1;
+    let max = house[N-1] - house[0];
 
-        
+    let result = 0;
+
+    while(min <= max){
+        const mid = Math.floor((min + max) / 2);
+
+        let cur_home = house[0];
+        let count = 1;
+
+        for(let i = 1; i < house.length; i++){
+            if(house[i] - cur_home >= mid){
+                count += 1;
+                cur_home = house[i];
+            }
+        }
+
+        if(count >= C) {
+            if(result < mid) {
+                result = mid;
+                min = mid + 1;
+            }
+        }
+        else {
+            max = mid -1;
+        }
     }
 
 
-    return input
+    return result;
 }
 
 const input =['5 3', 1, 2, 8, 4, 9]
